8dee1acd0f8b9dfda4b3669761f0e65a622d7b22
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-03 09:43:37 -0400

    implement redux

--- src/components/library.js
+++ src/components/library.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Library = props => {
+  const thing = useSelector(state => state.deckReducer)
+  console.log(thing)
+
   return (
     <section className="library">
     </section>
---
+++ src/reducers/deckReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  decks: []
+}
+
+const deckReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default deckReducer
