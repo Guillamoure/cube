59fdf3ae884d8334862dd6bcda176f28a5b61c9b
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-05 00:25:38 -0400

    shuffle library

--- src/actions/libraryActions.js
+++ src/actions/libraryActions.js
@@ -3,3 +3,4 @@ const { dispatch } = Store
 
 export const removeTopCard = () => dispatch({type: "REMOVE TOP CARD"})
 export const selectLibrary = (library) => dispatch({type: "SELECT LIBRARY", library})
+export const shuffle = () => dispatch({type: "SHUFFLE"})
--- src/containers/game.js
+++ src/containers/game.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { url } from '../helper_methods/variables'
 import { userData } from '../helper_methods/user_data'
 import { useSelector } from 'react-redux'
-import { selectLibrary } from '../actions/libraryActions'
+import { selectLibrary, shuffle } from '../actions/libraryActions'
 
 import Board from './board'
 
@@ -16,6 +16,7 @@ const Game = props => {
   let state = useSelector(state => state)
   if (!state.libraryReducer.activeLibrary.length && state.userReducers.user.name){
     selectLibrary(state.userReducers.user.decks[0])
+    shuffle()
   }
 
   return (
