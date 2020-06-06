0c8528ef3216e6887b3ee663512153ca128937cc
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-06 17:57:57 -0400

    shuffle library

--- src/modals/library_context_menu.js
+++ src/modals/library_context_menu.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 import { setModal } from '../actions/modalActions'
 import { addToHand } from '../actions/handActions'
-import { removeTopCard } from '../actions/libraryActions'
+import { removeTopCard, updateLibrary } from '../actions/libraryActions'
 import { addGraveyard } from '../actions/graveyardActions'
 import { draw } from '../helper_methods/library'
+import { shuffle } from '../helper_methods/functions/shuffle'
 
 const LibraryContextMenu = props => {
   const cmRef = React.useRef(null)
@@ -28,6 +29,11 @@ const LibraryContextMenu = props => {
     setModal(null, {}, {})
   }
 
+  const shuffleLibrary = () => {
+    updateLibrary(shuffle([...data]))
+    setModal(null, {}, {})
+  }
+
   const closeUp = () => {
     setModal("card", data.card, {})
   }
@@ -36,7 +42,7 @@ const LibraryContextMenu = props => {
     <ul ref={cmRef} id="context-menu" style={{top: coordinates.y, left: coordinates.x - selfWidth}}>
       <li className="context-menu-item" onClick={toHand}>Draw Card</li>
       <li className="context-menu-item" onClick={toGraveyard}>Mill</li>
-      <li className="context-menu-item"><del>Shuffle</del></li>
+      <li className="context-menu-item" onClick={shuffleLibrary}>Shuffle</li>
       <li className="context-menu-item"><del>Scry</del></li>
     </ul>
   )
