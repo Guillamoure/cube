6ed32e7be14be59ca45e68f0115dd8fc2d602966
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-30 00:26:04 -0400

    color gradient depending on chosen icons

--- src/containers/player-libraries/library-info.js
+++ src/containers/player-libraries/library-info.js
@@ -4,24 +4,35 @@ import { useSelector } from 'react-redux'
 const LibraryInfo = props => {
 
   const [libraryName, setName] = React.useState("")
+  const [selectedMana, setSelectedMana] = React.useState([])
 
   const opacity = mana => {
-
+    return selectedMana.includes(mana) ? "opaque-icon" : "transparant-icon"
   }
 
   const toggleMana = mana => {
-    
+    if (selectedMana.includes(mana)){
+      setSelectedMana(selectedMana.filter(m => m !== mana))
+    } else {
+      setSelectedMana([...selectedMana, mana])
+    }
+  }
+
+  const backgroundColors = () => {
+    if (selectedMana.length === 0){ return {backgroundColor: "beige", opacity: "0.8"} }
+    if (selectedMana.length === 1){ return {backgroundColor: selectedMana[0], opacity: "0.7"} }
+    if (selectedMana.length > 1){ return {backgroundImage: `linear-gradient(160deg, ${selectedMana.join(", ")})`, opacity: "0.7"} }
   }
 
   return (
-    <aside>
+    <aside style={backgroundColors()}>
       <input value={libraryName} placeholder="Library Name" onChange={e => setName(e.target.value)}/>
       <ul id="mana-icon-container">
-        <li><img clasName={opacity("black")} onClick={() => toggleMana("black")} src="/images/mana/black.png"/></li>
-        <li><img src="/images/mana/blue.png"/></li>
-        <li><img src="/images/mana/green.png"/></li>
-        <li><img src="/images/mana/red.png"/></li>
-        <li><img src="/images/mana/white.png"/></li>
+        <li><img className={opacity("black")} onClick={() => toggleMana("black")} src="/images/mana/black.png"/></li>
+        <li><img className={opacity("blue")} onClick={() => toggleMana("blue")} src="/images/mana/blue.png"/></li>
+        <li><img className={opacity("green")} onClick={() => toggleMana("green")} src="/images/mana/green.png"/></li>
+        <li><img className={opacity("red")} onClick={() => toggleMana("red")} src="/images/mana/red.png"/></li>
+        <li><img className={opacity("white")} onClick={() => toggleMana("white")} src="/images/mana/white.png"/></li>
       </ul>
     </aside>
   )
