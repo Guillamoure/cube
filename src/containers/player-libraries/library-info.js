6549f9b3f83b93b2fd439065326f613e8f66ae19
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-30 00:09:29 -0400

    setting up mana icon and creating aside list for library

--- src/containers/player-libraries/library-info.js
+++ src/containers/player-libraries/library-info.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const LibraryInfo = props => {
+
+  const [libraryName, setName] = React.useState("")
+
+  const opacity = mana => {
+
+  }
+
+  const toggleMana = mana => {
+    
+  }
+
   return (
     <aside>
+      <input value={libraryName} placeholder="Library Name" onChange={e => setName(e.target.value)}/>
+      <ul id="mana-icon-container">
+        <li><img clasName={opacity("black")} onClick={() => toggleMana("black")} src="/images/mana/black.png"/></li>
+        <li><img src="/images/mana/blue.png"/></li>
+        <li><img src="/images/mana/green.png"/></li>
+        <li><img src="/images/mana/red.png"/></li>
+        <li><img src="/images/mana/white.png"/></li>
+      </ul>
     </aside>
   )
 }
